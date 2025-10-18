e562b482f39d10172b97325dcf832b6965a75580
Hotwire <hotwire2990@gmail.com>
2025-10-18 15:58:38 -0700

    Update at 'Sat Oct 18 15:58:37 PDT 2025'

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -227,7 +227,7 @@ public class Robot extends LoggedRobot {
                   SmartDashboard.putBoolean(limelight + " detecting", true);
                   detectedFlag = true;
                   m_robotContainer.drivetrain.addVisionMeasurement(limelightMeasurement.pose,
-                    limelightMeasurement.timestampSeconds, VecBuilder.fill(1.0, 1.0, Double.POSITIVE_INFINITY));
+                    limelightMeasurement.timestampSeconds, VecBuilder.fill(Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY));
                   llestamation.setRobotPose(limelightMeasurement.pose);
               } else {
                   SmartDashboard.putBoolean(limelight + " detecting", false);
