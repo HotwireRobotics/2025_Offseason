d138092d3169e5b6034b065a72040b178bfdb259
Space Cat <spacecattis@gmail.com>
2026-01-07 18:21:26 -0800

    Fixed pathfinder

--- src/main/java/frc/robot/commands/Pathfind.java
+++ src/main/java/frc/robot/commands/Pathfind.java
@@ -10,40 +10,44 @@ import com.pathplanner.lib.path.PathPlannerPath;
 import com.pathplanner.lib.path.Waypoint;
 import com.pathplanner.lib.path.GoalEndState;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+
 public class Pathfind extends Command {
 
-    private final PathPlannerPath path;
+    private final Command follow;
 
     public Pathfind(Pose2d start, Pose2d end, PathConstraints constraints) {
-		
-        List<Waypoint> waypoints = PathPlannerPath.waypointsFromPoses(
-            start, end
+        List<Waypoint> waypoints = PathPlannerPath.waypointsFromPoses(start, end);
+
+        PathPlannerPath path = new PathPlannerPath(
+                waypoints,
+                constraints,
+                null,
+                new GoalEndState(0.0, end.getRotation())
         );
 
-        path = new PathPlannerPath(
-			waypoints,
-			constraints,
-			null,
-			new GoalEndState(0.0, end.getRotation())
-		);
-
         path.preventFlipping = true;
+
+        follow = AutoBuilder.followPath(path);
     }
 
     @Override
     public void initialize() {
+        follow.initialize();
     }
 
     @Override
     public void execute() {
+        follow.execute();
     }
 
     @Override
     public void end(boolean interrupted) {
+        follow.end(interrupted);
     }
 
     @Override
     public boolean isFinished() {
-        return true;
+        return follow.isFinished();
     }
 }
